7a0b2082249444669952363b3d881f4b13848ed8
Leon Stafford <me@ljs.dev>
2020-05-17 23:58:58 +0930

    test script + circleci

---
+++ README.md
@@ -0,0 +1,19 @@
+lokl-go
+=======
+
+Interactive script for launching and managing your [lokl](https://lokl.dev) websites.
+
+Usage
+=====
+
+### macOS, Linux
+
+`\curl -sSL https://lokl.dev/go | sh`
+
+
+Build status
+============
+
+[![CircleCI](https://circleci.com/gh/lokl-dev/go.svg?style=svg)](https://circleci.com/gh/lokl-dev/go)
+
+
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# lokl-go: shell script checker
+#
+# Tests that all shell scripts in lokl-go are POSIX-compliant and error-free
+#
+# This doesn't guarantee perfect functionality on every system, but ensures
+# the scripts themselves are free of common errors.
+#
+# License: The Unlicense, https://unlicense.org
+#
+# Usage: execute this script from the project root
+#
+#     $   sh tests.sh
+#
+
+# run shellcheck against all files in project, excluding .git dir
+find . -type f -not -path '*/\.git/*' -not -path '*/\.circleci/*' \
+  -exec shellcheck {} \;
