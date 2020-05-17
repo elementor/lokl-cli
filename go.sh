053a0c9256d745afa82ac92a1595809383fa57ec
Leon Stafford <me@ljs.dev>
2020-05-18 02:57:52 +0930

    wip create and manage

--- go.sh
+++ go.sh
@@ -22,6 +22,7 @@
 #
 
 main_menu() {
+  clear
   echo ""
   echo "====================================="
   echo "  Lokl launcher & management script  "
@@ -32,18 +33,123 @@ main_menu() {
   echo ""
   echo "q) Quit this menu"
   echo ""
-
+  echo ""
   echo "Please type (c), (m) or (q) and the Enter key: "
   echo ""
   read -r main_menu_choice
 
   if [ "$main_menu_choice" != "${main_menu_choice#[cmq]}" ] ;then
-    echo "Good choice, let's show you another menu!"
+    case $main_menu_choice in
+      c|C) create_site_choose_name ;;
+      m|M) manage_sites_menu ;;
+      q|Q) exit 0 ;;
+    esac
+
   else
     main_menu
   fi
 }
 
+create_site_choose_name() {
+  clear
+  echo ""
+  echo "================================================"
+  echo "      Lokl launcher & management script         "
+  echo "================================================"
+  echo "   Press (Ctrl) and (c) keys to exit anytime    "
+  echo "------------------------------------------------"
+  echo ""
+  echo "Choose a name for your new Lokl WordPress site. "
+  echo ""
+  echo "Please use letters, numbers and hyphens         "
+  echo ""
+  echo "ie, portfolio"
+  echo ""
+  echo ""
+  echo "Type your site name, then the Enter key: "
+  echo ""
+  read -r create_site_name_choice
+
+  # strip all non-alpha characters from string, convert to lowercase
+  LOKL_NAME="$(echo "$create_site_name_choice" | tr -cd '[:alnum:]-' | \
+    tr '[:upper:]' '[:lower:]')"
+
+  # trim hyphens from start and end and double-hyphens
+  LOKL_NAME="$(echo "$LOKL_NAME" | sed 's/--//g' | sed 's/^-//' | sed 's/-$//')"
+
+  # check name is not empty
+  if [ "$LOKL_NAME" = "" ]; then
+    create_site_choose_name
+  else
+    # trim to 100 chars if over
+    LOKL_NAME="$(echo "$LOKL_NAME" | cut -c1-100)"
+
+    echo "Launching your new Lokl WordPress site!"
+    echo "$LOKL_NAME"
+
+    LOKL_PORT=4996
+    LOKL_VERSION=0.0.2
+    docker run -e N="$LOKL_NAME" -e P="$LOKL_PORT" \
+      --name="$LOKL_NAME" -p "$LOKL_PORT":"$LOKL_PORT" \
+      -d lokl/lokl:"$LOKL_VERSION"
+
+   echo "Done! Access your Lokl WordPress site at:"
+   echo "http://$LOKL_NAME.localhost:$LOKL_PORT"
+   echo ""
+   echo "Press any key to manage sites:"
+
+   read -r ""
+    #read -r create_site_name_choice
+   manage_sites_menu
+  fi
+
+}
+
+manage_sites_menu() {
+  clear
+  echo ""
+  echo "================================================"
+  echo "      Lokl launcher & management script         "
+  echo "================================================"
+  echo "   Press (Ctrl) and (c) keys to exit anytime    "
+  echo "------------------------------------------------"
+  echo ""
+  echo "Your Lokl WordPress sites"
+  echo ""
+  # get all lokl container IDs
+  LOKL_CONTAINERS="$(docker ps -a | awk '{ print $1,$2 }' | grep lokl | awk '{print $1 }')"
+
+  SITE_COUNTER=1
+
+  # POSIX compliant way to iterate a list
+  OLDIFS="$IFS"
+  IFS='
+'
+  for CONTAINER_ID in $LOKL_CONTAINERS
+  do
+    CONTAINTER_NAME="$(docker inspect --format='{{.Name}}' "$CONTAINER_ID" | sed 's|/||')"
+    # get container's exposed port
+    CONTAINTER_PORT="$(docker inspect --format='{{.NetworkSettings.Ports}}' "$CONTAINER_ID" | \
+      sed 's/^[^{]*{\([^{}]*\)}.*/\1/' | awk '{print $2}')"
+
+    echo "$SITE_COUNTER)  http://$CONTAINTER_NAME.localhost:$CONTAINTER_PORT"
+
+    SITE_COUNTER=$((SITE_COUNTER+1))
+  done
+  IFS="$OLDIFS"
+
+  echo ""
+  echo "Choose the site you want to manage."
+  echo ""
+  echo "Type your site's number, then the Enter key: "
+  echo ""
+  # read -r site_to_manage_choice
+}
+
 main_menu
 
 exit 0
+
+
+
+
